4d5257b94a11bbafed1bc5791b4fa2ef2da12e9c
DijaniraMuachifi <167861614+DijaniraMuachifi@users.noreply.github.com>
2024-11-27 23:27:41 +0100

    I update the correct Jar File

---
+++ src/main/java/org/example/dijanira/model/Main.java
@@ -0,0 +1,7 @@
+package org.example.dijanira.model;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- src/main/java/org/example/dijanira/model/MainProgram.java
+++
@@ -1,9 +0,0 @@
-package org.example.dijanira.model;
-
-import org.example.dijanira.DijaniraApplication;
-
-public class MainProgram {
-    public static void main(String[] args) {
-        DijaniraApplication.main(args);
-    }
-}
