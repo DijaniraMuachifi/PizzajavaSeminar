1e7e8295e156ff5df74d02d8b376c1c137d28d93
DijaniraMuachifi <167861614+DijaniraMuachifi@users.noreply.github.com>
2024-11-27 22:53:39 +0100

    update jar file

---
+++ src/main/java/org/example/dijanira/model/MainProgram.java
@@ -0,0 +1,9 @@
+package org.example.dijanira.model;
+
+import org.example.dijanira.DijaniraApplication;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        DijaniraApplication.main(args);
+    }
+}
